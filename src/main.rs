4a3330b998e7f4aa44561f5875f69267b44d69e8
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-05-23 19:11:09 +0500

    working on rust

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    for number in 1..=3{
+        let pi:f32 = 3.14;
+        let name: &str = "ishaque";
+        println!("{}, My name is {}  {}", number,name, pi)
+
+    }
+    let numbers:[i32; 5]= [1,2,3,4,5];
+    // numbers[2] = 32;
+    let mut frusts: [String; 3] = ["apple".to_string(), "banana".to_string(), "orange".to_string()];
+    for ft in frusts.iter(){
+        println!("{}",ft);
+
+    }
+    
+    
+    println!("change string values apple to chary");
+    frusts[0]= "chary".to_string();
+    println!();
+    for f in frusts.iter(){
+        print!("{} ",f)
+    }
+
+    println!("{} on index 3",numbers[3]);
+
+}
+
